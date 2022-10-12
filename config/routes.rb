25389bff27f7b25a81c8f0dc13b127bb7599e99c
touya0215 <110541062+touya0215@users.noreply.github.com>
2022-10-12 12:03:46 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  #get 'post_images/new'
+  #get 'post_images/index'
+  #get 'post_images/show'
+  #上三列を下の一行に変更
+  resources :post_images, only: [:new, :index, :show]
+
   get '/homes/about' => 'homes#about', as: 'about'
   #as: Prefix Verb の変更 ec2-user:~/environment/meshiterro (main) $ rails routes にて確認できる。
 
 
   devise_for :users
   root to: "homes#top"
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
