38405b7443d6d458923d6c4687284f56db8b1e4b
touya0215 <110541062+touya0215@users.noreply.github.com>
2022-10-19 13:54:00 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,29 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  #get 'users/show'
-  #get 'users/edit'
-  #上二列を下の一行に変更
-  resources :users, only: [:show, :edit]
-  
-  
-  #get 'post_images/new'
-  #get 'post_images/index'
-  #get 'post_images/show'
-  #上三列を下の一行に変更
-  resources :post_images, only: [:new, :index, :show]
-
-  get '/homes/about' => 'homes#about', as: 'about'
-  #as: Prefix Verb の変更 ec2-user:~/environment/meshiterro (main) $ rails routes にて確認できる。
 
 
   devise_for :users
   root 'homes#top'
 
+  get '/homes/about' => 'homes#about', as: 'about'
+  #as: Prefix Verb の変更 ec2-user:~/environment/meshiterro (main) $ rails routes にて確認できる。
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+  #get 'users/show'
+  #get 'users/edit'
+  #上二列を下の一行に変更
+  resources :users, only: [:show, :edit]
+
+
+  #get 'post_images/new'
+  #get 'post_images/index'
+  #get 'post_images/show'
+  #上三列を下の一行に変更
+  #resources :post_images, only: [:new, :index, :show]
+
+
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
